50cbcd0c8e8a8db26a39a8494b2f2be7829a762a
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-05-24 23:45:12 +0430

    enviroment setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("API is running abc")
+})
+
+app.get("/api/chat", (req , res) => { 
+    res.send(chats)
+})
+
+app.get("/api/chat/:id", (req, res) => { 
+ //console.log(req.params.id)
+    const singlechat = chats.find(c => c._id === req.params.id)
+    res.send(singlechat)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000 , console.log(`server started on PORT ${PORT}`))
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import ChatPage from "./Pages/ChatPage";
+import HomePage from "./Pages/HomePage";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/chats" element={<ChatPage/>} />
+     </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
