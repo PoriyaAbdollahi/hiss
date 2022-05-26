69845acaf84aec3994626da543ba744275c4afc2
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-05-26 14:30:03 +0430

    setup database connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
 
+
+dotenv.config();
+
+connectDB()
 const app = express()
 
 
