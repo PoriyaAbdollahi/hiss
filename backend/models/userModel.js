21c8c2c22770adb954708496137a9ab20a0d8e2f
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-05-27 13:51:40 +0430

    user authentication (login and signup )aapi implemented

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose')
-
+const bcyrpt = require("bcryptjs")
 const userSchema = mongoose.Schema({
     name: {type:String , required:true }, 
-    email: {type:String , required:true } ,
+    email: { type: String, required: true ,unique:true } ,
     password: {type:String , required:true }, 
-    picture: {type:String , required:true ,default:"https://media.istockphoto.com/vectors/male-user-icon-vector-id517998264?b=1&k=20&m=517998264&s=170667a&w=0&h=CZ2MbiNDGXBTjFKA2Qd4Ly0FXIUkckBLhamwcUJaHG4="} 
+    picture: {type:String ,default:"https://media.istockphoto.com/vectors/male-user-icon-vector-id517998264?b=1&k=20&m=517998264&s=170667a&w=0&h=CZ2MbiNDGXBTjFKA2Qd4Ly0FXIUkckBLhamwcUJaHG4=" } 
 },
     { timestamps: true }
 
 )
+
+userSchema.methods.matchPassword = async function(enteredPassword){
+    console.log(this.password)
+    return await bcyrpt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre("save", async function(next){ 
+    if (!this.isModified) { 
+        next()
+    }
+    const salt = await bcyrpt.genSalt(10)
+    this.password = await bcyrpt.hash(this.password, salt)
+})
 const User = mongoose.model("User", userSchema)
 module.exports = User
--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,28 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 
 connectDB()
 const app = express()
 
+app.use(express.json()) // to accept JSON data
 
 app.get("/", (req, res) => {
     res.send("API is running abc")
 })
 
-app.get("/api/chat", (req , res) => { 
-    res.send(chats)
-})
 
-app.get("/api/chat/:id", (req, res) => { 
- //console.log(req.params.id)
-    const singlechat = chats.find(c => c._id === req.params.id)
-    res.send(singlechat)
-})
 
+
+app.use('/api/user', userRoutes)
+
+
+app.use(notFound)
+app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(5000 , console.log(`server started on PORT ${PORT}`))
