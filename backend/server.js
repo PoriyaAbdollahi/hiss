3bc6727114cbe74cb65065a3af6507e44408351f
PoriyaAbdollahi <poriyaa58@gmail.com>
2022-06-10 03:56:25 +0430

    server.js prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const chatRoutes = require("./routes/chatRoutes")
 const messageRoutes = require("./routes/messageRoutes")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const { off } = require("./models/userModel");
+const path = require('path')
 
 
 dotenv.config();
@@ -16,9 +17,6 @@ const app = express()
 
 app.use(express.json()) // to accept JSON data
 
-app.get("/", (req, res) => {
-    res.send("API is running abc")
-})
 
 
 
@@ -26,6 +24,21 @@ app.get("/", (req, res) => {
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRoutes)
+
+
+// deploy
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") { 
+    app.use(express.static(path.join(__dirname1, "frontend/build")));
+    app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    })
+} else {
+app.get("/", (req, res) => {
+    res.send("API is running abc")
+})
+ }
 
 
 app.use(notFound)
